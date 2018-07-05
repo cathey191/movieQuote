5eaa326ec7de79eb715f380d14c23236134d624c
cathey191 <cathey191@hotmail.com>
2018-07-06 10:06:49 +1200

    vue creating quotes

---
+++ js/script.js
@@ -0,0 +1,19 @@
+(function() {
+	var app2 = new Vue({
+		el: '#quote',
+		data: {
+			quotes: [
+				{
+					quote:
+						'You keep using that word. I do not think it means what you think it means.',
+					movie: 'The Princess Bride (1987)'
+				},
+				{
+					quote:
+						'Your mother was a hamster and your father smelt of elderberries.',
+					movie: 'Monty Python and the Holy Grail (1975)'
+				}
+			]
+		}
+	});
+})();
